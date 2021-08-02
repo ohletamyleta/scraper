c6fd2e901f1c6038d0c08f2eb3068483f228e986
ohletamyleta <ohletamyleta@gmail.com>
2021-08-02 13:12:03 -0400

    add iteration

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,23 @@ def scraper
   url = "https://blockwork.cc/"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
+  jobs = Array.new
+  job_listings = parsed_page.css('div.listingCard')
+  per_page = 
 
-  byebug
+
+  job_listings.each do |job_listing|
+    job = {
+      title: job_listing.css('span.job-title').text,
+      company: job_listing.css('span.company').text,
+      location: job_listing.css('span.location').text,
+      url: "https://blockwork.cc" + job_listing.css('a')[0].attributes["href].value"]
+    }
+   jobs << job
+  end
+
+
+
 end
 
 scraper
