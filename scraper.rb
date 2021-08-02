fc5a9b3c61825982fa0cc137989ef7bc15b00109
ohletamyleta <ohletamyleta@gmail.com>
2021-08-02 13:25:55 -0400

    finish iteration

--- scraper.rb
+++ scraper.rb
@@ -7,19 +7,26 @@ def scraper
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   jobs = Array.new
-  job_listings = parsed_page.css('div.listingCard')
-  per_page = 
+  job_listings = parsed_page.css('div.listingCard') #50 per page
 
+  per_page = job_listings.count 
+  total = parsed_page.css('div.job-count').text.split(' ')[1].gsub(',', '').to_i  #total jobs listed on site, varies
+  last_page = (total.to_f / per_page.to_f).round
+ 
+  while page <= last_page
+    pagination_url = "https://blockwork.cc/listings?page=#{page}"
+    job_listings.each do |job_listing|
+      job = {
+        title: job_listing.css('span.job-title').text,
+        company: job_listing.css('span.company').text,
+        location: job_listing.css('span.location').text,
+        url: "https://blockwork.cc" + job_listing.css('a')[0].attributes["href].value"]
+      }
+    jobs << job
+    end
+  page += 1
+end
 
-  job_listings.each do |job_listing|
-    job = {
-      title: job_listing.css('span.job-title').text,
-      company: job_listing.css('span.company').text,
-      location: job_listing.css('span.location').text,
-      url: "https://blockwork.cc" + job_listing.css('a')[0].attributes["href].value"]
-    }
-   jobs << job
-  end
 
 
 
